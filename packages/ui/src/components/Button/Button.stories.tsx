5a7b191882bb1c8ee666a7dd7a930e906fa0a8ee
Evgeny Glukhov <jgluhov@yandex.ru>
2026-04-11 23:57:22 +0300

    implement responsive buttons

--- packages/ui/src/components/Button/Button.stories.tsx
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -5,20 +5,22 @@ const meta = {
   component: Button,
   title: 'Button',
   tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const ContainedSmall: Story = {
-  render: () => <Button variant='contained' size='small'>Place bid</Button>
-};
-
-export const ContainedMedium: Story = {
-  render: () => <Button variant='contained' size='medium'>Place bid</Button>
-};
-
-export const ContainedLarge: Story = {
-  render: () => <Button variant='contained' size='large'>Place bid</Button>
+export const Contained: Story = {
+  render: () => (
+    <div  style={{ width: '80vw', display: 'flex', justifyContent: 'space-between' }}>
+      <Button variant='contained' size='small'>Place bid</Button>
+      <Button variant='contained' size='medium'>Place bid</Button>
+      <Button variant='contained' size='large'>Place bid</Button>
+    </div>
+    
+  )
 };
